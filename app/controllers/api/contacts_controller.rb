bac28145a46eb2ddd1df0a154b61dfaffe7e0d42
IMS-Global <brennick.sci@gmail.com>
2017-11-10 17:46:44 -0700

    WIP updated contacts_controller

--- app/controllers/api/contacts_controller.rb
+++ app/controllers/api/contacts_controller.rb
@@ -45,7 +45,8 @@ class Api::ContactsController < ApplicationController
   def contact_params
     params.require(:contact)
       .permit(
-        :id, :first, :last, :gender, :birthdate
+        :id, :first, :last, :gender, :birthdate,
+        :user_id, :created_at, :updated_at,
       )
   end
 end
